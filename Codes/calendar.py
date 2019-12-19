41faa050ab062c98cfc5dcb1d8673a4f50cad0ce
Nitin Sai <43436336+Nitin1901@users.noreply.github.com>
2019-12-19 16:32:42 +0530

    Update calendar.py

--- Codes/calendar.py
+++ Codes/calendar.py
@@ -1,4 +1,9 @@
+# Importing the calendar library.
 import calendar
+
+# Printing all months in a year.
+for name in calendar.month_name:
+    print(name)
 
 # TextCalendar class can be used to generate plain text calendars.
 c = calendar.TextCalendar(calendar.SUNDAY)
@@ -8,3 +13,14 @@ s = c.formatmonth(2019, 12, 0, 0)
 # Return a month’s calendar in a multi-line string. If w is provided, it specifies the width of the date columns, which are centered. If l is given, it specifies the number of lines that each week will use.
 
 print(s)
+
+# HTMLCalendar is used to print in HTML format.
+hc = calendar.HTMLCalendar(calendar.SUNDAY)
+s = hc.formatmonth(2019, 12)
+
+print(s)
+
+# We can print the days as well as the dates in a particular month by iterating through the object.
+for i in c.itermonthdays(2019, 12):
+    print(i)
+# itermonthdates() will return the dates in a particular month in a year.
